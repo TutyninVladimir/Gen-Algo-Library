6a1b584cdb445ca1c25c894f0072cfa17a8aed69
TutyninVladimir <tutynin.vladimir@yandex.ru>
2016-03-14 11:15:29 +0300

    Update GraphCreature.java

--- src/GraphCreature.java
+++ src/GraphCreature.java
@@ -66,7 +66,11 @@ abstract class GraphCreature extends Creature
 	}
 	public int[] get()
 	{
-		return this.a;
+		int i;
+		int[] tmp = new int[n];
+		for(i=0;i<n;i++)
+			tmp[i]=this.a[i];
+		return tmp;
 	}
 	public int getlenght()
 	{
@@ -96,8 +100,7 @@ abstract class GraphMutationFunction
 {
 	abstract void mutate(GraphCreature a);
 }
-
-class GraphCross extends GraphCrossFunction
+class SimpleGraphCross extends GraphCrossFunction
 {
 	void cross(GraphCreature a, GraphCreature b)
 	{
@@ -111,7 +114,8 @@ class GraphCross extends GraphCrossFunction
 			q1=q2;
 			q2=tmp;
 		}
-		int olda[],oldb[];
+		int[] olda;
+		int[] oldb;
 		olda=a.get();
 		oldb=b.get();
 		for(i=0;i<q1;i++)
@@ -128,6 +132,102 @@ class GraphCross extends GraphCrossFunction
 		}
 		a.set(olda);
 		b.set(oldb);
+		a.regenerate();
+		b.regenerate();
+	}
+}
+
+class ExtendedGraphCross extends GraphCrossFunction
+{
+	void cross(GraphCreature a, GraphCreature b)
+	{
+		int tmp,i,n;
+		n=a.getlenght();
+		int q1=1+((int)Math.random()*(a.getlenght()-1));
+		int q2=1+((int)Math.random()*(a.getlenght()-1));
+		if (q1==q2) q2++;
+		if (q1>q2)
+		{
+			tmp=q1;
+			q1=q2;
+			q2=tmp;
+		}
+		int olda[], newa[], oldb[], newb[];
+		olda=a.get();
+		oldb=b.get();
+		newa = new int[n];
+		newb = new int[n];
+		int p1=q2;
+		for(i=0;i<q1;i++)
+		{
+			newa[p1]=oldb[i];
+			newb[p1]=olda[i];
+			p1++;
+			if (p1>=n) p1-=n;
+			if (p1==q1) p1=q2;
+		}
+		for(i=q1;i<q2;i++)
+		{
+			newa[i]=olda[i];
+			newb[i]=oldb[i];
+		}
+		for(i=q2;i<n;i++)
+		{
+			newa[p1]=oldb[i];
+			newb[p1]=olda[i];
+			p1++;
+			if (p1>=n) p1=0;
+			if (p1==q1) p1=q2;
+		}
+		int[] cnt=new int[n];
+		for(i=0;i<n;i++)
+		{
+			cnt[i]=0;
+		}
+		p1=q1;
+		for(i=0;i<q1;i++)
+		{
+			if (cnt[newa[i]]==0) cnt[newa[i]]=1;
+			else 
+			{
+				newa[i]=oldb[p1];
+				p1++;
+			}
+		}
+		for(i=q2;i<n;i++)
+		{
+			if (cnt[newa[i]]==0) cnt[newa[i]]=1;
+			else 
+			{
+				newa[i]=oldb[p1];
+				p1++;
+			}
+		}
+		for(i=0;i<n;i++)
+		{
+			cnt[i]=0;
+		}
+		p1=q1;
+		for(i=0;i<q1;i++)
+		{
+			if (cnt[newb[i]]==0) cnt[newb[i]]=1;
+			else 
+			{
+				newb[i]=olda[p1];
+				p1++;
+			}
+		}
+		for(i=q2;i<n;i++)
+		{
+			if (cnt[newb[i]]==0) cnt[newb[i]]=1;
+			else 
+			{
+				newb[i]=olda[p1];
+				p1++;
+			}
+		}
+		a.set(newa);
+		b.set(newb);
 		a.regenerate();
 		b.regenerate();
 	}
@@ -156,6 +256,6 @@ class ExampleGraphCreature extends GraphCreature
 	double fit(int x[])
 	{
 		//TO DO: write function.
-		return x[1];
+		return x[0];
 	}
 }
